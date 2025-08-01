4be625531d91bd6798511a99661ff13f1cf5ee0a
Nurtay Tolymbek <nurtay.tolymbek@netcracker.com>
2025-08-01 12:46:57 +0500

    new method for InvalidClassifierException with default msg

--- dbaas/dbaas-aggregator/src/main/java/org/qubership/cloud/dbaas/exceptions/InvalidClassifierException.java
+++ dbaas/dbaas-aggregator/src/main/java/org/qubership/cloud/dbaas/exceptions/InvalidClassifierException.java
@@ -10,4 +10,10 @@ public class InvalidClassifierException extends ValidationException {
     public InvalidClassifierException(String detail, Map<String, Object> classifier, Source source) {
         super(ErrorCodes.CORE_DBAAS_4010, ErrorCodes.CORE_DBAAS_4010.getDetail(detail, classifier), source);
     }
+
+    public static InvalidClassifierException withDefaultMsg(Map<String, Object> classifier) {
+        return new InvalidClassifierException("Classifier doesn't contain all mandatory fields. " +
+                "Check that classifier has `microserviceName`, `scope`. If `scope` = `tenant`, classifier must contain `tenantId` property",
+                classifier, Source.builder().pointer("/classifier").build());
+    }
 }
